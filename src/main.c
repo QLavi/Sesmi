66323bf35c6bd7b2310b7c02840d3c890e9715e2
QLavi <LavenRose@protonmail.com>
2022-02-15 13:51:38 +0530

    added functions call to generate for specific pattern

--- src/main.c
+++ src/main.c
@@ -1,7 +1,15 @@
 #include "io_and_mem.h"
+#include "parser.h"
+#include "nfa.h"
 
 int main(void) {
-  maintain_allocations_for_me();
+  char pattern[] = "L.a.v.i.(R|E)*.w.e*";
+  size_t size = sizeof(pattern);
+  AST_Node* node = pattern_to_ast(pattern, size);
+  write_ast_in_dot(node);
+
+  NFA_State* state = ast_to_nfa(node);
+  write_nfa_in_dot(state);
 
   FREE_ALL();
   return 0;
