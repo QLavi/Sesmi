3fe791513f0b78d7546a4d5c72831f76dd340fd7
QLavi <LavenRose@protonmail.com>
2022-02-15 13:47:01 +0530

    added |, ?, +, * operators

--- src/nfa.c
+++ src/nfa.c
@@ -37,6 +37,7 @@ void generate_nfa(AST_Node* node) {
       NFA_State* start = make_state(node->c, 'e', NULL, NULL);
       NFA_State* final = make_state('e', 'e', NULL, NULL);
       start->next[0] = final;
+
       NFA_Fragment frag = {start, final};
       PUSH(frag);
     } break;
@@ -52,19 +53,53 @@ void generate_nfa(AST_Node* node) {
     } break;
     case NODE_ALTER:
     {
+      NFA_Fragment f1 = POP();
+      NFA_Fragment f0 = POP();
 
+      NFA_State* start = make_state('e', 'e', f0.start, f1.start);
+      NFA_State* final = make_state('e', 'e', NULL, NULL);
+
+      f0.final->next[0] = final;
+      f1.final->next[0] = final;
+
+      NFA_Fragment frag = {start, final};
+      PUSH(frag);
     } break;
     case NODE_QMARK:
     {
+      NFA_Fragment f0 = POP();
 
+      NFA_State* start = make_state('e', 'e', f0.start, NULL);
+      NFA_State* final = make_state('e', 'e', NULL, NULL);
+      f0.final->next[0] = final;
+      start->next[1] = final;
+
+      NFA_Fragment frag = {start, final};
+      PUSH(frag);
     } break;
     case NODE_PLUS:
     {
+      NFA_Fragment f0 = POP();
 
+      NFA_State* state = make_state('e', 'e', f0.start, NULL);
+      f0.final->next[0] = f0.start;
+      f0.final->next[1] = make_state('e', 'e', NULL, NULL);
+
+      NFA_Fragment frag = {state, f0.final->next[1]};
+      PUSH(frag);
     } break;
     case NODE_STAR:
     {
+      NFA_Fragment f0 = POP();
 
+      NFA_State* state = make_state('e', 'e', f0.start, NULL);
+      NFA_State* final = make_state('e', 'e', NULL, NULL);
+      f0.final->next[0] = f0.start;
+      f0.final->next[1] = final;
+      state->next[1] = final;
+
+      NFA_Fragment frag = {state, final};
+      PUSH(frag);
     } break;
   }
 }
