c81fb5879de5ac51a5f107ef66a51e9822783cff
QLavi <LavenRose@protonmail.com>
2022-02-16 21:09:05 +0530

    added proper result printing and error checking

--- run_test.py
+++ run_test.py
@@ -7,9 +7,13 @@ def load_file(filename):
 contents = load_file("tests/regex.txt")
 contents = [x.rsplit() for x in contents]
 
-error_descr = []
 for test_exprs in contents:
     print(f"./play {test_exprs[0]}")
     capture = subproc.run(['./play', test_exprs[0]], capture_output=True)
     got = str(capture.stderr)
-    error_descr.append(got)
+    if got != "b''":
+      print("===FAILED===")
+      print(got)
+    else:
+      print("===PASSED===")
+
