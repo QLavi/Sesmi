944f4a535a0b629a6799a12efc828b961bea576d
QLavi <LavenRose@protonmail.com>
2022-02-17 16:59:58 +0530

    added functions to simulate NFA

--- include/nfa.h
+++ include/nfa.h
@@ -7,9 +7,12 @@ typedef struct NFA_State NFA_State;
 struct NFA_State {
   char c[2];
   int id;
+  int last_list; // is in accepting state or not
+  bool is_final;
   bool reached[2];
   NFA_State* next[2];
 };
 
 NFA_State* ast_to_nfa(AST_Node* node);
 void write_nfa_in_dot(NFA_State* state);
+bool simulate_nfa(NFA_State* start, char* match);
--- src/nfa.c
+++ src/nfa.c
@@ -18,6 +18,8 @@ NFA_State* make_state(char c0, char c1, NFA_State* s0, NFA_State* s1) {
   state->c[1] = c1;
   state->id = state_id;
   state_id += 1;
+  state->last_list = 0;
+  state->is_final = false;
   state->reached[0] = false;
   state->reached[1] = false;
   state->next[0] = s0;
@@ -125,11 +127,16 @@ void generate_nfa(AST_Node* node) {
   }
 }
 
+NFA_State final_state = {.c[0] = 'F', .is_final = true};
 NFA_State* ast_to_nfa(AST_Node* node) {
   generate_nfa(node);
   NFA_Fragment frag = POP();
+
+  final_state.id = state_id;
+  state_id += 1;
+
   if(*frag.final == NULL) {
-    *frag.final = make_state('e', 'e', NULL, NULL);
+    *frag.final = &final_state;
   }
   return frag.start;
 }
@@ -156,4 +163,75 @@ void write_nfa_in_dot(NFA_State* state) {
   write_state_ids(fptr, state);
   dwrite("}\n");
   fclose(fptr);
+}
+
+typedef struct {
+  NFA_State** states;
+  int count;
+}State_List;
+
+int list_id = 0;
+void add_state(State_List* list, NFA_State* state);
+
+State_List* start_list(NFA_State* state) {
+  State_List* list = ALLOC(State_List, 1);
+  list->states = ALLOC(NFA_State*, state_id);
+  list->count = 0;
+  if(state == NULL) {
+    return list;
+  }
+  list_id += 1;
+  add_state(list, state);
+  return list;
+}
+
+void add_state(State_List* list, NFA_State* state) {
+  if(state == NULL || state->last_list == list_id) return;
+
+  state->last_list = list_id;
+  if(state->next[0] != NULL && state->next[1] != NULL) {
+    add_state(list, state->next[0]);
+    add_state(list, state->next[1]);
+    return;
+  }
+  list->states[list->count] = state;
+  list->count += 1;
+}
+
+void nfa_step(State_List* current_states, State_List* next_states, int c) {
+  list_id += 1;
+  next_states->count = 0;
+
+  for(int x = 0; x < current_states->count; x++) {
+    NFA_State* state = current_states->states[x];
+
+    if(state->c[0] == c) {
+      add_state(next_states, state->next[0]);
+    }
+    if(state->c[1] == c){
+      add_state(next_states, state->next[1]);
+    }
+  }
+}
+
+bool simulate_nfa(NFA_State* start, char* match) {
+
+  State_List* current_states = start_list(start);
+  State_List* next_states = start_list(NULL);
+
+  char* p = match;
+  for(;*p; p++) {
+    nfa_step(current_states, next_states, *p);
+
+    State_List* tmp = current_states;
+    current_states = next_states;
+    next_states = tmp;
+  }
+  
+  for(int x = 0; x < current_states->count; x++) {
+    if(current_states->states[x] == &final_state) {
+      return 1;
+    }
+  }
+  return 0;
 }
