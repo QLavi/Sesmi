059dfca9a98e4187e25b7b41c4e3f26b9a499595
QLavi <LavenRose@protonmail.com>
2022-02-17 23:39:49 +0530

    commented dot graph function call

--- src/main.c
+++ src/main.c
@@ -4,18 +4,20 @@
 #include "nfa.h"
 
 int main(int argc, char** argv) {
-  if(argc != 2) {
+  if(argc != 3) {
     fprintf(stderr, "Usage: ./play <regex> <string_to_match>\n");
     exit(1);
   }
-  char* string = argv[2];
   char* pattern = argv[1];
+  char* string = argv[2];
   size_t size = strlen(pattern);
   AST_Node* node = pattern_to_ast(pattern, size);
   write_ast_in_dot(node);
 
   NFA_State* state = ast_to_nfa(node);
-  write_nfa_in_dot(state);
+  bool accepted = simulate_nfa(state, string);
+  printf("%i\n", accepted);
+  /* write_nfa_in_dot(state); */
 
   FREE_ALL();
   return 0;
