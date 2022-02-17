a87183ce2e10ff69710398c5e119dba4f9ce5012
QLavi <LavenRose@protonmail.com>
2022-02-17 16:58:14 +0530

    updated tests expression and test runner for better output

--- run_test.py
+++ run_test.py
@@ -1,3 +1,4 @@
+from pprint import pprint 
 import subprocess as subproc
 
 def load_file(filename):
@@ -5,15 +6,21 @@ def load_file(filename):
         return f.readlines()
 
 contents = load_file("tests/regex.txt")
-contents = [x.rsplit() for x in contents]
+contents = [x.split(' ') for x in contents]
+contents = [[x[0], x[1], x[2].rsplit()[0]] for x in contents]
+test_args = [[x[0], x[1]] for x in contents]
+expected = [int(x[2]) for x in contents]
 
-for test_exprs in contents:
-    print(f"./play {test_exprs[0]}")
-    capture = subproc.run(['./play', test_exprs[0]], capture_output=True)
-    got = str(capture.stderr)
-    if got != "b''":
-      print("===FAILED===")
-      print(got)
-    else:
-      print("===PASSED===")
+for idx, (arg0, arg1) in enumerate(test_args):
+  capture = subproc.run(['./play', arg0, arg1], capture_output=True)
+  got = capture.stdout
+
+  result = got[0] - 48
+  if expected[idx] == result:
+    print("[PASSED] ", end='')
+  else:
+    print("[FAILED] ", end='')
+
+  print(arg0, arg1)
+
 
